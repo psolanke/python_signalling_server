95aedac3e68234f352ffdefba6ae4fd960f2dea6
psolanke <pratiksolankes@gmail.com>
2018-12-08 16:49:12 +0530

    New test client

--- static/js/testclient.js
+++ static/js/testclient.js
@@ -1,13 +1,18 @@
-var socket;
-document.addEventListener("DOMContentLoaded", function(event) { 
+$(document).ready(function(){
   namespace = '/signalling';
   socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
-});
+  socket.on('sdp_message', function(msg){
+    console.log(msg.sdp_message);
+  });
 
-function get_msg(){
-  socket.emit('get_response',{'data':'This String will be returned'});
-  socket.on('response', function(msg) {
-            console.log(msg.message);
-          });
-}
+  $('form#register_as_user').submit(function(event) {
+    socket.emit('register_endpoint_server', {id: $('#register_as_user_data').val(), type:'endpoint'});
+    return false;
+  });
+
+  $('form#contact_endpoint_server').submit(function(event) {
+    socket.emit('contact_endpoint_server', {endpoint_server_id: $('#endpoint_server_id_data').val(), sdp_message:$('#sdp_data').val()});
+    return false;
+  });
+});
 
