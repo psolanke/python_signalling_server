6bdef4d7c4add6044b4d1f569925db3bc309d4d0
psolanke <pratiksolankes@gmail.com>
2018-12-23 18:06:36 +0530

    stability issue solved

--- main_app.py
+++ main_app.py
@@ -1,45 +1,52 @@
 from flask import Flask, render_template, jsonify
 from flask_socketio import SocketIO, emit, join_room, rooms
-# from storage_utils import StorageManager
+from threading import Lock
+
+SIGNALLING_NAMESPACE = '/signalling'
+TEST_NAMESPACE = '/test'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'PLACEHOLDER'
 
-socketio = SocketIO(app, async_mode='eventlet')
+socketio = SocketIO(app, async_mode=None)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/testclient')
 def test_client():
-	return render_template('testclient.html')
+    return render_template('testclient.html')
 
-@socketio.on('connect', namespace='/signalling')
+@socketio.on('disconnect', namespace=SIGNALLING_NAMESPACE)
+def test_disconnect():
+    print('Client disconnected')
+
+@socketio.on('connect', namespace=SIGNALLING_NAMESPACE)
 def connected():
-	print('Connected')
-	emit('response',{'message': 'Connection Successful!!'})
+    print('Connected')
+    emit('response',{'message': 'Connection Successful!!'})
 
-@socketio.on('get_response', namespace='/signalling')
+@socketio.on('get_response', namespace=SIGNALLING_NAMESPACE)
 def test_response(message):
-	print(message['data'])
-	emit('response',{'message': message['data']})
+    print(message['data'])
+    emit('response',{'message': message['data']})
 
-@socketio.on('register_endpoint_server', namespace='/signalling')
+@socketio.on('register_endpoint_server', namespace=SIGNALLING_NAMESPACE)
 def register_user(message):
-	print(message['id'])
-	print(message['type'])
-	room = message['id']
-	join_room(room)
-	emit('response',{'message':'In rooms: ' + ', '.join(rooms())})
+    print(message['id'])
+    print(message['type'])
+    room = message['id']
+    join_room(room)
+    emit('response',{'message':'In rooms: {}'.format(room)})
 
-@socketio.on('contact_endpoint_server', namespace='/signalling')
+@socketio.on('contact_endpoint_server', namespace=SIGNALLING_NAMESPACE)
 def get_endpoint_server(message):
-	print(message['endpoint_server_id'])
-	room = message['endpoint_server_id']
-	sdp_message = message['sdp_message']
-	emit('sdp_message', {'sdp_message':sdp_message}, room=room)
+    print(message['endpoint_server_id'])
+    room = message['endpoint_server_id']
+    sdp_message = message['sdp_message']
+    emit('sdp_message', {'sdp_message':sdp_message}, room=room)
 
 
 if __name__ == '__main__':
-	socketio.run(app, debug=True)
+    socketio.run(app, debug=True)
